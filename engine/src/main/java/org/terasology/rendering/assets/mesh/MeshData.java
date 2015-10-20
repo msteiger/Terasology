98b33ede8e723b7f4c26cbdf5a9011a7e46f4a33
Martin Steiger <git@martin-steiger.de>
2015-10-20 17:44:39 +0200

    Multi-group OBJ mesh loader

--- engine/src/main/java/org/terasology/rendering/assets/mesh/MeshData.java
+++ engine/src/main/java/org/terasology/rendering/assets/mesh/MeshData.java
@@ -19,6 +19,12 @@ import gnu.trove.list.TFloatList;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TFloatArrayList;
 import gnu.trove.list.array.TIntArrayList;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.terasology.assets.AssetData;
 
 /**
@@ -32,6 +38,8 @@ public class MeshData implements AssetData {
     private TFloatList normals = new TFloatArrayList();
     private TFloatList colors = new TFloatArrayList();
     private TIntList indices = new TIntArrayList();
+
+    private Map<String, IndexRange> groups = new LinkedHashMap<>();
 
     public MeshData() {
     }
@@ -58,5 +66,81 @@ public class MeshData implements AssetData {
 
     public TIntList getIndices() {
         return indices;
+    }
+
+    /**
+     * Adds a (face) group. If it already exists, this will overwrite previous settings.
+     * @param groupId the group id
+     * @param startIndex the first index of the group
+     */
+    public void startGroupAt(String groupId, int startIndex) {
+        groups.put(groupId, new IndexRange(startIndex));
+    }
+
+    /**
+     * Sets the end index of a (face) group. If it already exists, this will overwrite previous settings.
+     * @param groupId the group id
+     * @param endIndex the first index of the group
+     */
+    public void endGroupAt(String groupId, int endIndex) {
+        IndexRange group = groups.get(groupId);
+        group.setLength(endIndex + 1 - group.getMin());
+    }
+
+    /**
+     * Finds the start index of a (face) group
+     * @param groupId the group id
+     * @return the first vertex index
+     */
+    public int getGroupStart(String groupId) {
+        return groups.get(groupId).getMin();
+    }
+
+    /**
+     * Finds the length a (face) group
+     * @param group the group id
+     * @return the first vertex index
+     */
+    public int getGroupLength(String group) {
+        return groups.get(group).getLength();
+    }
+
+    /**
+     * An unmodifiable set of group IDs
+     * @return the group IDs.
+     */
+    public Set<String> getGroupIds() {
+        return Collections.unmodifiableSet(groups.keySet());
+    }
+
+    private static class IndexRange {
+        private int length;
+        private int min;
+
+        public IndexRange(int min) {
+            this.min = min;
+            this.length = 0;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public int getMin() {
+            return min;
+        }
+
+        public int getMax() {
+            return (min + length - 1);
+        }
+
+        @Override
+        public String toString() {
+            return "IndexRange [" + min + ".." + getMax() + "]";
+        }
     }
 }
