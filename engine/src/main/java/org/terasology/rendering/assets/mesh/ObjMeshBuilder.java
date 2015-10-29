d8815b0d0564a896f3ba7fdc5f4bbc784b9eb5fe
Martin Steiger <git@martin-steiger.de>
2015-10-29 21:20:08 +0100

    Use external obj loader library

---
+++ engine/src/main/java/org/terasology/rendering/assets/mesh/ObjMeshBuilder.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.rendering.assets.mesh;
+
+import gnu.trove.list.TIntList;
+import de.javagl.obj.AbstractWritableObj;
+import de.javagl.obj.FloatTuple;
+import de.javagl.obj.ObjFace;
+
+/**
+ * An adapter for de.javagl.obj.ObjReader that spits out MeshData instances.
+ */
+class ObjMeshBuilder extends AbstractWritableObj {
+
+    private final MeshData result = new MeshData();
+
+    public MeshData getMeshData() {
+        return result;
+    }
+
+    @Override
+    public void addVertex(FloatTuple vertex) {
+        result.getVertices().add(vertex.getX());
+        result.getVertices().add(vertex.getY());
+        result.getVertices().add(vertex.getZ());
+    }
+
+    @Override
+    public void addTexCoord(FloatTuple texCoord) {
+        result.getTexCoord0().add(texCoord.getX());
+        result.getTexCoord0().add(1 - texCoord.getY());
+    }
+
+    @Override
+    public void addNormal(FloatTuple normal) {
+        result.getNormals().add(normal.getX());
+        result.getNormals().add(normal.getY());
+        result.getNormals().add(normal.getZ());
+    }
+
+    @Override
+    public void addFace(ObjFace face) {
+        TIntList indices = result.getIndices();
+        for (int i = 0; i < face.getNumVertices() - 2; i++) {
+            indices.add(face.getVertexIndex(0));
+            indices.add(face.getVertexIndex(i + 1));
+            indices.add(face.getVertexIndex(i + 2));
+        }
+    }
+}
